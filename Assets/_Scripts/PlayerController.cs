9cfc1f456cc5b7985be9f1b069c861f717ddeeed
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-02-11 22:43:02 +0100

    Player movement

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private Rigidbody rbody;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rbody.velocity = movement * speed;
+        rbody.position = new Vector3(
+            Mathf.Clamp(rbody.position.x, boundary.xMin, boundary.xMax),
+            0f,
+            Mathf.Clamp(rbody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rbody.rotation = Quaternion.Euler(0f, 0f, rbody.velocity.x * -tilt);
+    }
+
+}
