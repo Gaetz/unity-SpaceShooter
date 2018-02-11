fc044b964adbe4a36105a0739189e69d8b32e8ce
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-02-11 23:01:49 +0100

    Laser bolt

---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        rbody.velocity = speed * rbody.transform.forward;
+    }
+}
