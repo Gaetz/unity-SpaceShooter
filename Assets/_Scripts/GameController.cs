b197886117c2537cbb8662e1bd5da2bfd52da202
Gaetz <gaetz@rufflerim.com>
2018-02-22 12:11:26 +0100

    Astroid waves and cleaning explosions

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -6,19 +6,32 @@ public class GameController : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hasardCount;
 
-	void Start () {
-        SpawnWaves();
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+
+    void Start () {
+        StartCoroutine(SpawnWaves());
 	}
 	
 	void Update () {
 		
 	}
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        yield return new WaitForSeconds(startWait);
         Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        while (true) {
+            for (int i = 0; i < hasardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
