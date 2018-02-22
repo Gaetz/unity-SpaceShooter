ad5c9a1494c183ca2ecd65ee8443730472d81288
Gaetz <gaetz@rufflerim.com>
2018-02-22 11:35:51 +0100

    Rotating astroid and destroy it by shooting

---
+++ Assets/_Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+    Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody>();
+        rbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
